98f3678d701e20798914640878889491f4b75401
WhiteF326 <Lume1a326.leno@gmail.com>
2021-06-10 16:04:30 +0900

    submit_01

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const hostname="localhost";
 const port = 810;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.writeHead(200, {"Content-Type" : "text/plain"});
+  switch(req.url){
+    case "/submit":
+      msg = "submit";
+      break;
+    default:
+      msg = "res";
+      break;
+  }
+  res.write(msg);
+  res.end();
 });
 
 server.listen(port, hostname, () => {
