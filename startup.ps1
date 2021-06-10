e1f724f4cb796fbf9cbbb1233d843aeaad558cb1
WhiteF326 <Lume1a326.leno@gmail.com>
2021-06-10 20:19:53 +0900

    k

--- startup.ps1
+++ startup.ps1
@@ -1,11 +1,11 @@
-if (gcm deno -ea SilentlyContinue){
-  if(gcm denon -ea SilentlyContinue){
-    echo ":) ready to boot"
+if (Get-Command deno -ea SilentlyContinue){
+  if(Get-Command denon -ea SilentlyContinue){
+    Write-Output ":) ready to boot"
   }else{
     deno install --allow-read --allow-run --allow-write --allow-net -f -q --unstable https://deno.land/x/denon@2.4.0/denon.ts
   }
 }else{
-  iwr https://deno.land/x/install/install.ps1 -useb | iex
+  Invoke-WebRequest https://deno.land/x/install/install.ps1 -useb | Invoke-Expression
   deno install --allow-read --allow-run --allow-write --allow-net -f -q --unstable https://deno.land/x/denon@2.4.0/denon.ts
 }
-denon run -A server.js
+denon start
