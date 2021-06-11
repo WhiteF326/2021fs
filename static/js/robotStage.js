8c8a8657edb95b5dc967327bde3787fcf1f89ee4
WhiteF326 <Lume1a326.leno@gmail.com>
2021-06-12 02:36:08 +0900

    move char

--- static/js/robotStage.js
+++ static/js/robotStage.js
@@ -8,6 +8,14 @@ class RobotStage {
     this.height = height;
     this.cellsize = cellsize;
 
+    this.hasPlayer = false;
+    this.playerX = 0;
+    this.playerY = 0;
+
+    this.pendingMoveX = 0;
+    this.pendingMoveY = 0;
+    this.moveSpeedPerFrame = 4;
+
     this.stage = [];
     this.stageCanvas = document.createElement("canvas");
     this.stageCanvas.id = "roboCanvas";
@@ -19,7 +27,7 @@ class RobotStage {
     for (let i = 0; i < height; i++) {
       this.stage.push([]);
       for (let j = 0; j < width; j++) {
-        this.stage[i].push(0);
+        this.stage[i].push(null);
       }
     }
 
@@ -34,6 +42,49 @@ class RobotStage {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, ct.width, ct.height);
 
+    if(this.hasPlayer){
+      // move direction at this time
+      let mdx = 0, mdy = 0;
+      if(this.pendingMoveX > 0) mdx = 1 * this.moveSpeedPerFrame;
+      else if(this.pendingMoveX < 0) mdx = -1 * this.moveSpeedPerFrame;
+      if(this.pendingMoveY > 0) mdy = 1 * this.moveSpeedPerFrame;
+      else if(this.pendingMoveY < 0) mdy = -1 * this.moveSpeedPerFrame;
+
+      this.stage[this.playerX][this.playerY].move(mdx, mdy);
+      this.pendingMoveX -= mdx, this.pendingMoveY -= mdy;
+    }
+
+    for(let i = 0; i < this.height; i++){
+      for(let j = 0; j < this.width; j++){
+        if(this.stage[i][j]){
+          this.stage[i][j].render(ct, this.cellsize, this.cellsize);
+        }
+      }
+    }
+
     requestAnimationFrame(this._mainLoop.bind(this));
+  }
+
+  add(obj, x, y, isPlayer){
+    this.stage[x][y] = obj;
+    if(isPlayer){
+      this.playerX = x, this.playerY = y;
+      this.hasPlayer = true;
+    }
+  }
+
+  movePlayer(vx, vy){
+    const tx = this.playerX + vx;
+    const ty = this.playerY + vy;
+    if(tx >= 0 && tx < this.width && ty >= 0 && ty < this.height){
+      //  move player
+      this.pendingMoveX += vx * this.cellsize;
+      this.pendingMoveY += vy * this.cellsize;
+      const tmp = this.stage[this.playerX][this.playerY];
+      this.stage[this.playerX][this.playerY] = this.stage[tx][ty];
+      this.stage[tx][ty] = tmp;
+      this.playerX = tx;
+      this.playerY = ty;
+    }
   }
 }
--- static/js/sprite.js
+++ static/js/sprite.js
@@ -1,18 +1,29 @@
 'use strict'
 
 class Sprite {
-	constructor( img ) {
-		this.img = new Image();
-		this.img.src = img;
-		this.x = this.y = 0;
-	}
+  constructor(img) {
+    this.img = new Image();
+    this.img.src = img;
+    this.x = this.y = 0;
+  }
 
-	update( canvas ) {
-		this.render( canvas );
-	}
+  update(canvas, x, y) {
+    this.render(canvas, x, y);
+  }
 
-	render( canvas ) {
-		const _ctx = canvas.getContext( '2d' );
-		_ctx.drawImage( this.img, this.x, this.y );
-	}
+  render(canvas, w, h) {
+    const _ctx = canvas.getContext('2d');
+    _ctx.drawImage(
+      this.img,
+      0, 0,
+      this.img.naturalWidth, this.img.naturalHeight,
+      this.x, this.y,
+      w, h
+    );
+  }
+
+  move(x, y){
+    this.x += x;
+    this.y += y;
+  }
 }
