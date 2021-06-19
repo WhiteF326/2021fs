e4c52d10cef1c605d231d8f8a5e4d779f110a0d3
WhiteF326 <Lume1a326.leno@gmail.com>
2021-06-19 13:41:06 +0900

    a

--- static/js/sprite.js
+++ static/js/sprite.js
@@ -1,15 +1,27 @@
 'use strict'
 
 class Sprite {
-  constructor(img, type) {
+  constructor(img, type, vector) {
     this.img = new Image();
-    this.img.src = img;
+    this.img.src = null;
     this.x = this.y = 0;
     this.type = type;
+    if(type == "Player"){
+      console.log(img);
+      this.imglist = img;
+      this.img.src = img[vector];
+      this.vector = vector;
+    }else{
+      this.img.src = img;
+    }
   }
 
   render(canvas, wh) {
     const _ctx = canvas.getContext('2d');
+    if(this.type == "Player"){
+      // update vector
+      this.img.src = this.imglist[this.vector];
+    }
     _ctx.drawImage(
       this.img,
       0, 0,
@@ -26,5 +38,9 @@ class Sprite {
 
   gettype(){
     return this.type;
+  }
+
+  setvector(vector){
+    this.vector = vector;
   }
 }
